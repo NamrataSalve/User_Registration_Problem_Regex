33c2273a7301527491ec1d575a1c1d145373f08f
NamrataSalve <namratasalve1999@gmail.com>
2024-10-10 19:30:18 +0530

    [Namrata] Added: Welcome massage for User registration validation problem

---
+++ src/com/userregistrationvalidation/UserRegistrationValidation.java
@@ -0,0 +1,7 @@
+package com.userregistrationvalidation;
+
+public class UserRegistrationValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Validation Problem.. ");
+    }
+}
